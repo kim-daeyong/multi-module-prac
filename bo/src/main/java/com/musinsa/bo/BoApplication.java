e2d6b43bc707f8877614b30ce8732aaab29f19b9
kim-daeyong <kdy2353@gmail.com>
2024-07-17 23:51:09 +0900

    init

---
+++ bo/src/main/java/com/musinsa/bo/BoApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoApplication.class, args);
+    }
+
+}
