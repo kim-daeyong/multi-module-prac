6c008eedf7f5d3520b59b89148c788afe8638038
kim-daeyong <kdy2353@gmail.com>
2024-07-19 12:45:09 +0900

    chore(querydsl) add querydsl config

--- core/src/main/java/com/musinsa/core/common/config/db/jpa/JpaConfig.java
+++ core/src/main/java/com/musinsa/core/common/config/db/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.musinsa.core.common.config.jpa;
+package com.musinsa.core.common.config.db.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
