39ea6baa7dbdf9427dac545f28c2010606ca6389
kim-daeyong <kdy2353@gmail.com>
2024-07-21 20:21:47 +0900

    refactor(responseMessage) add msg and code in ResponseMessage

--- core/src/main/java/com/musinsa/core/common/message/ResponseMessage.java
+++ core/src/main/java/com/musinsa/core/common/message/ResponseMessage.java
@@ -1,16 +1,28 @@
 package com.musinsa.core.common.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
 public class ResponseMessage<T> {
+    private String resultMsg;
+
+    private String resultCode;
+
     private T data;
 
     public ResponseMessage(T data) {
+        this.resultMsg = ResponseCode.SUCCESS.getMessage();
+        this.resultCode = ResponseCode.SUCCESS.getCode();
+        this.data = data;
+    }
+
+    public ResponseMessage(ResponseCode responseCode, T data) {
+        this.resultMsg = responseCode.getMessage();
+        this.resultCode = responseCode.getCode();
         this.data = data;
     }
 }
