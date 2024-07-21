e1e2314c45d4783ec01f85ee50aab87b57624b74
kim-daeyong <kdy2353@gmail.com>
2024-07-21 22:45:58 +0900

    chore(deploy) add docker-compose for deploy

--- core/src/main/java/com/musinsa/core/common/config/redis/RedisConfig.java
+++ core/src/main/java/com/musinsa/core/common/config/redis/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -27,15 +28,11 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-//    @Value("${spring.data.redis.password}")
-//    private String password;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
-//        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
 
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
